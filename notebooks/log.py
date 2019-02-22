fcc572a2f0ded917245daa22a46b0c64259f8ed8
Jonas Avrin <jawabiscuit@users.noreply.github.com>
2019-02-21 19:12:16 -0500

    Add notebooks

---
+++ notebooks/log.py
@@ -0,0 +1,15 @@
+In[12]
+_i
+_ii
+print 'last input:', _i
+print 'next one  :', _ii
+print 'and next  :', _iii
+%history -n 1-5
+!pwd
+import os
+os.getcwd()
+files = !ls
+print "My current directory's files:"
+print files
+%history?
+%history -n 1-5
